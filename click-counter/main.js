eb34817a6cfa58da529f454c1c1a499be982aba6
Harris Escalante <11287035+harrisfoes@users.noreply.github.com>
2024-08-22 22:40:01 +0900

    investigation

--- click-counter/main.js
+++ click-counter/main.js
@@ -6,13 +6,34 @@ const divCounter = document.getElementById("counter");
 
 const countNumber = document.getElementById("count-number");
 
-const btn = document.getElementById("button");
+const increaseBtn = document.getElementById("increase-button");
 
-btn.addEventListener("click", clickCounter());
+const resetBtn = document.getElementById("reset-button");
 
-console.log(countNumber);
+const decreaseBtn = document.getElementById("decrease-button");
 
-function clickCounter() {
-  countNumber.textContent = `You have clicked this window ${timesClicked} times`;
+console.log(timesClicked);
+
+function increaseCounter() {
   timesClicked++;
+  console.log("inc");
+  console.log(timesClicked);
+  countNumber.textContent = `You have clicked this window ${timesClicked} times`;
 }
+
+function decreaseCounter() {
+  timesClicked--;
+  console.log("dec");
+  console.log(timesClicked);
+  countNumber.textContent = `You have clicked this window ${timesClicked} times`;
+}
+function resetCounter() {
+  timesClicked = 0;
+  console.log("res");
+  console.log(timesClicked);
+  countNumber.textContent = `You have clicked this window ${timesClicked} times`;
+}
+
+increaseBtn.addEventListener("click", increaseCounter());
+decreaseBtn.addEventListener("click", decreaseCounter());
+resetBtn.addEventListener("click", resetCounter());
